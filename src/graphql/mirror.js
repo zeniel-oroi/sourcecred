791316b586aa870e05fa3bd6c089c65f58cdfb22
William Chargin <wchargin@gmail.com>
2018-09-12 19:07:28 -0700

    _createUpdate

--- src/graphql/mirror.js
+++ src/graphql/mirror.js
@@ -204,7 +204,19 @@ export class Mirror {
       }
     });
   }
+
+  /**
+   * Register a new update, representing one communication with the
+   * remote server. A unique ID will be created and returned.
+   */
+  _createUpdate(updateTimestamp: Date): UpdateId {
+    return this._db
+      .prepare("INSERT INTO updates (time_epoch_millis) VALUES (?)")
+      .run(+updateTimestamp).lastInsertROWID;
+  }
 }
+
+type UpdateId = number;
 
 /**
  * Execute a function inside a database transaction.
